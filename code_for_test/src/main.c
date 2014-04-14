8d481b02909e29f9bcf633250d85252d8ab5c494
colin_g <colin_g@epitech.eu>
2014-04-14 19:18:55 +0200

    Adding test for list insert

--- code_for_test/src/main.c
+++ code_for_test/src/main.c
@@ -5,7 +5,7 @@
 ** Login  <colin_g@epitech.eu>
 **
 ** Started on  lun. avril 14 04:59:52 2014 julien colin
-** Last update lun. avril 14 06:49:10 2014 julien colin
+** Last update lun. avril 14 19:17:15 2014 julien colin
 */
 
 #include <stdio.h>
@@ -42,6 +42,7 @@ void		test_push_front(t_list *list_string, t_list *list_int, char *s)
         printf("%d\n", (int)node_int->data);
         node_int = node_int->next;
     }
+    printf("\n");
 }
 
 void		test_push_back(t_list *list_string, t_list *list_int, char *s)
@@ -74,8 +75,43 @@ void		test_push_back(t_list *list_string, t_list *list_int, char *s)
         printf("%d\n", (int)node_int->data);
         node_int = node_int->next;
     }
+    printf("\n");
 }
 
+void		test_insert(t_list *list_string, t_list *list_int, char *s)
+{
+    char*	p;
+    t_node*	node_string;
+    t_node*	node_int;
+    int		i;
+
+    i = 0;
+    printf("<< __ %s __ >>\n", __FUNCTION__);
+    p = strtok(s, " ");
+    while (p)
+    {
+        printf("list_string insert at %d [%s]\n", i % 2, (char *)p);
+        printf("list_int    insert at %d [%d]\n", i % 2, (int)p);
+        insert(list_string, p, i % 2);
+        insert(list_int, p, i++ % 2);
+        p = strtok(0, " ");
+    }
+    printf("Content of node_string :\n");
+    node_string = list_string->front;
+    while (node_string)
+    {
+        printf("%s\n", (char*)node_string->data);
+        node_string = node_string->next;
+    }
+    printf("Content of node_int :\n");
+    node_int = list_int->front;
+    while (node_int)
+    {
+        printf("%d\n", (int)node_int->data);
+        node_int = node_int->next;
+    }
+    printf("\n");
+}
 
 int	main(int ac, char **av)
 {
@@ -99,5 +135,14 @@ int	main(int ac, char **av)
     list_destroy(&list_string);
     list_destroy(&list_int);
     free(s2);
+
+    list_init(&list_string);
+    list_init(&list_int);
+    s2 = strdup(av[ac > 1 ? 1 : 0]);
+    test_insert(&list_string, &list_int, s2);
+    list_destroy(&list_string);
+    list_destroy(&list_int);
+    free(s2);
+
     return (0);
 }
